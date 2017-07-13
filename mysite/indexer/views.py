5355923842875135c9ffddee67c260fe973d2cb3
gaguiarsdev <aguiarsantos.25@gmail.com>
2017-07-13 18:47:38 -0300

    Finished the HTML need to format the css and put a input so user can change the username they wanna follow

--- mysite/indexer/views.py
+++ mysite/indexer/views.py
@@ -1,16 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from tweepy import Stream
-from tweepy import OAuthHandler
-from tweepy.streaming import StreamListener
-import TwitterApi
+from twitterapi import views
+
 
 # Create your views here.
 
-urlArray = TwitterApi.getImageUrlFile("hideo_kojima_en")
+urlArray = views.getImageUrlFile("hideo_kojima_en")
 
 print (urlArray)
 
 def indexer(request):
-    return HttpResponse("A")
-
+     return render(request, 'indexer/home.html', {'urlArray': urlArray })
---
+++ mysite/mysite/TwitterApi.py
@@ -0,0 +1,42 @@
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import time
+import tweepy
+import requests
+
+ckey = 'odv8ArZCY8jYF1I0ouA6YhUkc'
+csecret = 'uCItp88lB7JXx9KjOSFmucieBbdDmDQV2pGgyszs7NmDsjV5GI'
+atoken = '3003780227-dSNYMNV0rSagWlXFQKThVj2FbpdRiDKkkKGNimf'
+asecret = 'O94WtGID6f41FuifTBy6ddm6uNyG5s2BvYUXNghEnk5mc'
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+
+def getImageUrlFile(sentUser):
+
+    api = tweepy.API(auth)
+    urlArray = []
+
+    user = api.get_user(sentUser)
+    print (user.screen_name)
+
+    filename = user.screen_name+".txt"
+
+    print (filename)
+    saveFile = open(filename,'w')
+
+
+
+    public_tweets = api.user_timeline(screen_name = user.screen_name,count=200)
+    for tweet in public_tweets:
+        if 'media' in tweet.entities:
+            for image in  tweet.entities['media']:
+                urlArray.append(image['media_url'])
+                data = image['media_url']
+                saveFile.write(data)
+                saveFile.write('\n')
+
+    saveFile.close()
+    return urlArray
